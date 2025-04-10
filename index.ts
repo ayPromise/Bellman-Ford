a90f9916dec3656338fbdf05fb28a566df61af92
ayPromise servant <fatherpother@gmail.com>
2025-04-10 14:20:42 +0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+interface Edge{
+    from:string;
+    to:string;
+    weight:number;
+}
+
+interface Graph {
+    verticles:number;
+    edges:Array<Edge>
+}
+
+function bellmanFord(input: Graph){
+    
+}
